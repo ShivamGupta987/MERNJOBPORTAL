df732fdca6700f77cd5383f0ad19b288e2d5a850
ShivamGupta987 <118923085+ShivamGupta987@users.noreply.github.com>
2024-03-21 10:17:48 +0530

    env file added

--- job-portal-client/src/firebase/firebase.config.js
+++ job-portal-client/src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB8ce8avl9he8vMH4iKTaq6MX7dVfotP70",
-  authDomain: "job-portal-client-be8c5.firebaseapp.com",
-  projectId: "job-portal-client-be8c5",
-  storageBucket: "job-portal-client-be8c5.appspot.com",
-  messagingSenderId: "656844916395",
-  appId: "1:656844916395:web:c28978afc6bf1bbfa799ee"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket:import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+  appId:import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
