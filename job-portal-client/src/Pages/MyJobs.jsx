0b825e3c7ae1f577e9763026a58806b6a67f40bf
SHIVAM GUPTA <118923085+ShivamGupta987@users.noreply.github.com>
2024-03-26 00:06:48 -0700

    Myjob error solving

--- job-portal-client/src/Pages/MyJobs.jsx
+++ job-portal-client/src/Pages/MyJobs.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 
 const MyJobs = () => {
-  // const email = "sg804595@gmail.com"
+
 
   const [jobs, setJobs] = useState([]);
   const [searchText, setSearchText] = useState("");
@@ -54,14 +54,12 @@ const MyJobs = () => {
 
   const handleSearch = () =>{
       const filter = jobs.filter((job) => job.jobTitle.toLowerCase().indexOf(searchText.toLowerCase()) !== -1);
-    //   console.log(filter)
     setJobs(filter)
-    // if data filter setisloading false 
     setIsLoading(false)
 
   }
   const handleDelete = (id) => {
-    // console.log(id)
+
     fetch(`http://localhost:5000/job/${id}`,{
       method : 'DELETE'
     }).then(res => res.json).then(data => {
@@ -71,7 +69,7 @@ const MyJobs = () => {
       }
     })
   }
-//   console.log(searchText)
+
   return (
     <div className="max-w-screen-2xl container mx-auto xl:px-24 px-4">
   
